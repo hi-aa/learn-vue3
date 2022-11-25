339174a41df93392ebee98f7ce0ac3c769f527e9
core_user10 <ost0119@coremethod.co.kr>
2022-11-25 22:03:53 +0900

    Vue lectures 3: Options API vs Composition API

---
+++ src/components/lectures/3/App_OptionsAPI.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			counter: 10,
+		};
+	},
+	methods: {
+		increment() {
+			this.counter++;
+		},
+	},
+	mounted() {
+		console.log('mounted');
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
