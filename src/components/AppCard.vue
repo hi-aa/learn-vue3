5961635e40838594d678783a95ead11138fed82b
hi-aa <ost.jj1201@gmail.com>
2022-12-03 16:21:25 +0900

    props, emit

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,50 +1,91 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<span class="badge bg-secondary">
+				<!-- {{type === 'news' ? '뉴스' : '공지사항'}} -->
+				{{ typeName }}
+			</span>
+			<h5 class="card-title mt-2">{{ title }}</h5>
 			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ content }}
 			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+
+			<!-- <a v-if="isLike" href="#" class="btn btn-danger">좋아요</a> -->
+			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike"
+				>좋아요</a
+			>
 		</div>
+
+		<!-- props에 접근 가능
+		{{ $props }} -->
 	</div>
 </template>
 
 <script>
-import { ref, useCssModule } from 'vue';
+import { computed } from 'vue';
 
 export default {
-	setup() {
-		// console.log('app card setup');
+	// props: ['title', 'content'], //외부로부터 받아올 데이터
+	// props의 선언은 배열도 되고 객체도 됨
+	// 공식 가이드는 객체로 상세하게 설정하는것을 권유함..
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				// 유효성 검사
+				return ['news', 'notice'].includes(value); // false가 나오면 콘솔에 경고가 뜸
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
 
-		// style에 module 속성을 넣어서 사용하는 경우
-		// const style = useCssModule();
-		// console.log(style); // {red: '_red_13fes_3'} // 스크립트로 가져와서 사용 가능
+		// Object 타입의 props는 default 선언 시 함수로
+		obj: {
+			type: Object,
+			default: () => {
+				return {};
+			},
+		},
+	},
+	// props는 단방향 데이터 흐름
+	// 상위속성에서 변경하면 하위속성에 업데이트 됨
+	// 반대는 X
 
-		const varColor = ref('red');
-		return { varColor };
+	// 하위 속성에서 값을 변경하고 싶다면 상위속성으로 이벤트를 호춠시켜서
+	// 상위속성에서 값을 변경할 수 있도록
+	// emits을 이용해 구현할 수 있음
+	emits: ['toggleLike'],
+	setup(props, context) {
+		// setup의 매개변수로 props를 받을 수 있음 // 첫번째 매개변수
+		// context가 두번째 매개변수
+		// console.log(props.title);
+
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+		);
+		const typeName = computed(() =>
+			props.type === 'news' ? '뉴스' : '공지사항',
+		);
+
+		const toggleLike = () => {
+			context.emit('toggleLike'); // 상위속성에서 toggleLike라는 이벤트를 실행시킴
+			// console.log(props.isLike);
+
+			// 자바스크립트 특성 상 자식 엘리먼트에서도 객체나 배열의 속성을 변경 가능하긴 함
+			// 그래도 그렇게 수정하지 말고 context.emit으로 부모요소에서 업데이트 하도록 할 것
+		};
+		return { isLikeClass, typeName, toggleLike };
 	},
 };
 </script>
-
-<!-- <style lang="scss" scoped></style> -->
-
-<!-- style에 module 속성을 넣으면 css 클래스를 $style 객체의 속성으로 노출한다
-:class="$style.red" // 이렇게 추가 가능 -->
-<!-- <style module> -->
-
-<!-- module에 이름 지정가능
-:class="classes.red" 로 사용 가능 -->
-<!-- <style module="classes">
-.red {
-	color: red !important;
-}
-</style> -->
-
-<!-- style에 v-bind를 이용해서 값을 설정할 수 있음 -->
-<!-- <style>
-.red {
-	color: v-bind(varColor) !important;
-}
-</style> -->
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,20 +1,36 @@
 <template>
 	<main>
 		<div class="container py-4">
-			<div class="col col-4">
-				<AppCard></AppCard>
-			</div>
-			<div class="col col-4">
-				<AppCard></AppCard>
-			</div>
-			<div class="col col-4">
-				<AppCard></AppCard>
+			<div class="row g-3">
+				<!-- <div class="col col-4">
+					<AppCard title="제목1" content="내용1"></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div> -->
+				<div class="col col-4" v-for="(post, index) in posts" :key="index">
+					<AppCard
+						:title="post.title"
+						:content="post.content"
+						:type="post.type"
+						:is-like="post.isLike"
+						@toggle-like="post.isLike = !post.isLike"
+					></AppCard>
+					<!-- :isLike="post.isLike"
+                    둘다 가능한데 공식문서는 케밥케이스 권유 -->
+				</div>
+
+				<div class="col col-4">
+					<!-- disabled로 넣어두는게 :disabled="true"와 동일함 -->
+					<AppCard title="제목1" content="내용1" disabled></AppCard>
+				</div>
 			</div>
 		</div>
 	</main>
 </template>
 
 <script>
+import { reactive } from 'vue';
 import AppCard from './AppCard.vue';
 
 export default {
@@ -22,7 +38,14 @@ export default {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const posts = reactive([
+			{ title: '제목1', content: '내용1', type: 'news', isLike: true },
+			{ title: '제목2', content: '내용2', type: 'notice', isLike: false },
+			{ title: '제목3', content: '내용3', type: 'news', isLike: true },
+			{ title: '제목4', content: '내용4', type: 'news', isLike: true },
+			{ title: '제목5', content: '내용5', type: 'news', isLike: true },
+		]);
+		return { posts };
 	},
 };
 </script>
