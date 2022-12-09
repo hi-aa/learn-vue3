bf1ce8cbd4e12053815d59f70209164c08fae3dc
hi-aa <ost.jj1201@gmail.com>
2022-12-09 14:16:44 +0900

    suspense

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="container py-4">
 		<!-- <TemplateRef></TemplateRef> -->
-		<!-- <ScriptSetup></ScriptSetup> -->
+		<Suspense>
+			<ScriptSetup></ScriptSetup>
+			<template #fallback>로딩중...</template>
+		</Suspense>
 
-		<DynamicComponent></DynamicComponent>
+		<!-- <DynamicComponent></DynamicComponent> -->
 	</div>
 </template>
 
 <script setup>
-// import ScriptSetup from './ScriptSetup.vue';
-import DynamicComponent from './DynamicComponent.vue';
+import ScriptSetup from './ScriptSetup.vue';
+// import DynamicComponent from './DynamicComponent.vue';
 </script>
 
 <style lang="scss" scoped></style>
